b777950a3610a939a5f9ce476c5236924f2023b0
DeivisonSouza <souzaflorestal@gmail.com>
2021-09-10 17:23:10 -0300

    Update TCM class

--- Slides/duncan.test.R
+++ Slides/duncan.test.R
@@ -1,33 +1,18 @@
 library(dplyr)
 library(agricolae)
 
-data <- readr::read_csv("Slides/data/DIC-Pimentel-Gomes-2002.csv")
-
-data_pivot <- data %>%
-  tidyr::pivot_longer(
-    cols = starts_with("P"),
-    names_to = "Progenie",
-    values_to = "Valor",
-    names_transform = list(Progenie = as.factor),
-    values_drop_na = TRUE
-  ) %>%
-  rename(Repeticao = `Rep./Prog.`) %>%
-  arrange(Progenie, Repeticao)
+(data <- labestData::BanzattoQd3.7.1)
 
 # Realizar a análise de variância
-res.aov <- aov(Valor ~ Progenie, data = data_pivot)
+res.aov <- aov(alt ~ comp, data = data)
 
 # Resumo da análise de variância
 summary(res.aov)
 
 # Teste de Duncan
-out.duncan <- duncan.test(res.aov,"Progenie", group=TRUE,
+out.duncan <- duncan.test(res.aov,"comp", group=TRUE,
                           alpha=0.05, console=TRUE)
-out.duncan$parameters
-# Teste Tukey
-out.tukey <- HSD.test(res.aov, "Progenie", group=TRUE,
-                      console=TRUE, main="")
-out.tukey$parameters
+
 # Gráfico Duncan
 plot(out.duncan, variation="IQR")
 
