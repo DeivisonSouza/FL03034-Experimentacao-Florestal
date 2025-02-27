16ae8c39457ea0aa0e951d154a0b2039913a142c
Deivison Souza <souzaflorestal@gmail.com>
2025-02-27 11:25:11 -0300

    Update

--- 02 - AED.R
+++
@@ -1,5 +0,0 @@
-############# Análise Exploratória de Dados ###########
-# Nome: Deivison Venicio Souza 😀                  ####
-# E-mail= deivisonvs@ufpa.br                       ####
-# Instituição: Universidade Federal do Pará - UFPA ####
-#######################################################
